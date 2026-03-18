78074fee3362656f6aa315ee1f6fb72dfbc507ac
Perumal408 <perumal.s@ladybirdweb.com>
2026-03-18 14:52:39 +0530

    breadcrums_issue_composer_fix

--- config/breadcrumbs.php
+++ config/breadcrumbs.php
@@ -4,6 +4,10 @@ return [
 
     'view' => '_partials.breadcrumbs',
 
-    'files' => base_path('app/Http/breadcrumbs.php'),
+    'files' => [],
+
+    'unnamed-route-exception' => false,
+    'missing-route-bound-breadcrumb-exception' => false,
+    'invalid-named-breadcrumb-exception' => false,
 
 ];
